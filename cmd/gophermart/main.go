2e5a59042152423c85dfb706afd124a8b24a6902
Michael Rechkunov <mrechkunov@gmail.com>
2026-03-25 12:05:44 +0300

    autotest test

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"gofermart/interal/config"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	config.Init()
+	log.Println("reading config")
+	r := chi.NewRouter()
+	//	r.Post("/", handler.PostHandler)
+	//	r.Get("/{id}", handler.GetHandler)
+	log.Println("starting web server at:", config.ConfigAdreses.ServerBindAdress)
+	err := http.ListenAndServe(config.ConfigAdreses.ServerBindAdress, r)
+	if err != nil {
+		panic(err)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module gofermart
 
 go 1.26.1
+
+require github.com/go-chi/chi/v5 v5.2.5
